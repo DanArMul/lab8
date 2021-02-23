e9cd7f9b6cb58f80a1b2aa03093f967a743bc367
Daniel Muljono <dmuljono@ucsd.edu>
2021-02-23 22:06:21 +0700

    Third party API lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'Z9jMzqlHveNEOS8ATtELnJYwZPIQ9MOQ';
+
+	// 'map' refers to a <div> element with the ID map
+	L.mapquest.map('map', {
+	  center: [32.87852911015582, -117.23588967260979],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12
+	});
 }
